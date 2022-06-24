d2d83557eae90174a4605fc4f52d60a3fb8b1fc4
afrivera <andresrfelip@hotmail.com>
2022-06-24 11:09:07 -0500

    config middleware valid-fields

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 const { newUser, login, getToken } = require('../controllers/auth');
+const { validFields } = require('../middlewares/valid-fields');
 
 const router = Router();
 
@@ -9,13 +10,14 @@ router.post('/new', [
     check('name', 'name is required').not().isEmpty(),
     check('email', 'email no valid').isEmail(),
     check('password', 'password is required').isLength({ min: 6 }),
+    validFields
 ],newUser)
 
 // login
 router.post('/', [
     check('email', 'email no valid').isEmail(),
     check('password', 'password is required').isLength({ min: 6 }),
-
+    validFields
 ],login )
 
 // validate token
