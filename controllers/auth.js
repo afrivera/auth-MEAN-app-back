a99ccba83c10166126068364f3a3e45737cfa604
afrivera <andresrfelip@hotmail.com>
2022-06-24 11:02:24 -0500

    config field required in routes

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,28 @@
 const { request, response } = require("express");
+const { validationResult } = require("express-validator")
 
 const newUser = ( req = request, res = response )=> {
+    const errors = validationResult( req );
+    if(!errors.isEmpty()){
+        return res.status(400).json({
+            ok: false,
+            errors: errors.mapped()
+        })
+    }
     const { name, email, password } = req.body;
     return res.json({ ok: true, msg: 'create'});
 }
 
 const login = ( req = request, res = response )=> {
+
+    const errors = validationResult( req );
+    if(!errors.isEmpty()){
+        return res.status(400).json({
+            ok: false,
+            errors: errors.mapped()
+        })
+    }
+
     const { email, password } = req.body;
     return res.json({ ok: true, msg: 'login'});
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,22 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
 const { newUser, login, getToken } = require('../controllers/auth');
 
 const router = Router();
 
 // create user
-router.post('/new', newUser)
+router.post('/new', [
+    check('name', 'name is required').not().isEmpty(),
+    check('email', 'email no valid').isEmail(),
+    check('password', 'password is required').isLength({ min: 6 }),
+],newUser)
 
 // login
-router.post('/', login )
+router.post('/', [
+    check('email', 'email no valid').isEmail(),
+    check('password', 'password is required').isLength({ min: 6 }),
+
+],login )
 
 // validate token
 router.get('/renew', getToken )
